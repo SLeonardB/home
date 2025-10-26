ba119af0e13567bd2d21b210b611cbc8bc0afa8f
SLeonardB <sleonardbilly23@gmail.com>
2025-10-27 00:45:50 +0300

    Added smooth scroll effects

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@
 const toggle = document.getElementById('nav-toggle');
 const links = document.getElementById('nav-links');
 toggle.addEventListener('click', () => links.classList.toggle('open'));
+
+// Smooth scroll effect
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
