64ddee4a8df05f6dae44e89376c83b9856903e94
SLeonardB <sleonardbilly23@gmail.com>
2025-10-27 00:39:07 +0300

    Created initial main.js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Mobile menu toggle
+const toggle = document.getElementById('nav-toggle');
+const links = document.getElementById('nav-links');
+toggle.addEventListener('click', () => links.classList.toggle('open'));
